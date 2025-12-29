83f7ae1a2ad676519d710d6c35a991895136e4c0
anand kishor gupta <anandgupta20588@gmail.com>
2025-12-29 15:28:39 +0545

    remove black color

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,38 +1,3 @@
-// import { useEffect, useState } from "react";
-// import BOD from "../components/BOD/BOD";
-// import Hero from "../components/Hero/Hero";
-// import HowWeDo from "../components/HowWeDo/HowWeDo";
-// import NoticeModal from "../components/Modal/NoticeModal";
-// import WhatWeDo from "../components/WhatWeDo/WhatWeDo";
-// import OurServices from "../components/ourServices/OurServices";
-// import WhyUs from "../components/whyUs/WhyUs";
-
-// const Home = () => {
-//   const [open, setOpen] = useState(false);
-
-//   // Runs every time Home page loads or reloads
-//   useEffect(() => {
-//     setOpen(true);
-//   }, []);
-//   return (
-//     <div>
-//       <NoticeModal open={open} onClose={() => setOpen(false)} />
-//       <div
-//         className={`transition duration-300 ${open ? "blur-md pointer-events-none select-none" : ""
-//           }`}
-//       >
-//       </div>
-//       <Hero />
-//       <WhyUs />
-//       <WhatWeDo />
-//       <OurServices />
-//       <HowWeDo />
-//       <BOD />
-//     </div>
-//   );
-// };
-
-// export default Home;
 
 
 import { useEffect, useState } from "react";
